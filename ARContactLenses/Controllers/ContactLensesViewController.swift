c27d02fb14e9a8a9c94fd2e6f5f1aad30f5cbec0
Leonardo Garcia <leo.equih@gmail.com>
2019-04-09 17:13:03 -0500

    Clear ContactLensesViewController implementation to explain it

--- ARContactLenses/Controllers/ContactLensesViewController.swift
+++ ARContactLenses/Controllers/ContactLensesViewController.swift
@@ -13,11 +13,9 @@ final class ContactLensesViewController: UIViewController {
     /// Set renderer
     private let sceneView = ARSCNView(frame: .zero)
 
-    private var leftEyeNode: ImageNode?
-    private var rightEyeNode: ImageNode?
+    /// Declare eye nodes
 
     /// Specify ARConfiguration
-    private let faceTrackingConfiguration = ARFaceTrackingConfiguration()
 
     override func loadView() {
         super.loadView()
@@ -27,21 +25,20 @@ final class ContactLensesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         guard ARFaceTrackingConfiguration.isSupported else { fatalError("A TrueDepth camera is required") }
-        // Set sceneView delegate
-        sceneView.delegate = self
+        /// Set sceneView delegate
+
         /// If needed, we can set some debugOptions
+
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         /// Run session
-        sceneView.session.run(faceTrackingConfiguration)
     }
 
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         /// Pause session
-        sceneView.session.pause()
     }
 }
 
@@ -51,41 +48,32 @@ extension ContactLensesViewController: ARSCNViewDelegate {
         // Note: - You must compile this project with an iPhone with TrueDepth camera as target device, otherwise it will mark that sceneView has no `device` member
 
         /// Validate anchor is an ARFaceAnchor instance
-        guard anchor is ARFaceAnchor,
-            let device = sceneView.device else { return nil }
+
 
         /// Create node with face geometry
-        let faceGeometry = ARSCNFaceGeometry(device: device)
-        let node = SCNNode(geometry: faceGeometry)
 
-        node.geometry?.firstMaterial?.colorBufferWriteMask = []
 
-        let irisImage = #imageLiteral(resourceName: "bicolor_iris")
         /// Create eye ImageNodes
-        rightEyeNode = ImageNode(width: 0.015, height: 0.015, image: irisImage)
-        leftEyeNode = ImageNode(width: 0.015, height: 0.015, image: irisImage)
+
 
         /// Change the origin of the eye nodes
-        rightEyeNode?.pivot = SCNMatrix4MakeTranslation(0, 0, -0.01)
-        leftEyeNode?.pivot = SCNMatrix4MakeTranslation(0, 0, -0.01)
+
 
         /// Add child nodes
-        rightEyeNode.flatMap { node.addChildNode($0) }
-        leftEyeNode.flatMap { node.addChildNode($0) }
 
-        return node
+        return nil
     }
 
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        guard let faceAnchor = anchor as? ARFaceAnchor,
-            let faceGeometry = node.geometry as? ARSCNFaceGeometry else { return }
+        /// Get face anchor as ARFaceAnchor
+
 
         /// Update the geometry displayed on screen to be now conformed by the anchor calculated
-        faceGeometry.update(from: faceAnchor.geometry)
+
 
         /// Update node transforms
-        leftEyeNode?.simdTransform = faceAnchor.leftEyeTransform
-        rightEyeNode?.simdTransform = faceAnchor.rightEyeTransform
+
+
     }
 }
 
