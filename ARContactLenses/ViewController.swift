ccdeaa8b61fff6db65bad06dfcdef2bd0a7fb34d
Leonardo Garcia <leo.equih@gmail.com>
2019-04-08 23:42:27 -0500

    Initial Commit

---
+++ ARContactLenses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARContactLenses
+//
+//  Created by Leonardo Garcia  on 4/8/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
